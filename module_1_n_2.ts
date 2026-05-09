42a90da5a044b7d60c8b21f1a6758da75505eb63
Md Mustafizur Rahman Sayem <40750318+rahmancoder@users.noreply.github.com>
2026-05-09 13:15:23 +0000

    variables

---
+++ module_1_n_2.ts
@@ -0,0 +1,10 @@
+// javaScript is a dynamically typed language.
+// let a  = 5;
+// a = "Mustafiz"
+
+// variables in  TS
+let shopName: string = "Mustafiz Store";
+let totalProducts: number = 50;
+let isOpen: boolean = true;
+let rating: null = null;
+let discount: undefined = undefined;
