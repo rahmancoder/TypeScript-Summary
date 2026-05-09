0841e0ac457ccf983b708b0f0dd2b229e1a024e8
Md Mustafizur Rahman Sayem <40750318+rahmancoder@users.noreply.github.com>
2026-05-09 13:17:06 +0000

    non promitive variables

--- module_1_n_2.ts
+++ module_1_n_2.ts
@@ -2,9 +2,37 @@
 // let a  = 5;
 // a = "Mustafiz"
 
-// variables in  TS
+// primitive variables in  TS
 let shopName: string = "Mustafiz Store";
 let totalProducts: number = 50;
 let isOpen: boolean = true;
 let rating: null = null;
 let discount: undefined = undefined;
+
+
+// non-primitive
+let baseProducts: string[] = ["Ghee", "Egg", "Salt"];
+
+// let prices: number[] = [60,150,40]
+
+const products: {
+  name: string;
+  price: number;
+  category: "grocery" | "electronics";
+  discount?: number;
+} = {
+  name: "Rice",
+  price: 60,
+  category: "grocery",
+};
+
+// void
+// function voidfunc(): void{
+//     console.log("this is void")
+// }
+
+function calculateTotal(price: number, quantity: number): number {
+  return price * quantity;
+}
+
+// console.log(calculateTotal(50, 5)); // 250
